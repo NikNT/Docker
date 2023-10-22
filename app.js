81ca3935e957fc066d8d41d94b44b35416180bc9
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-10-22 17:44:09 -0400

    5. Docker Compose

--- app.js
+++ app.js
@@ -6,9 +6,21 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hey World!");
+  res.json([
+    {
+      id: "1",
+      title: "Book Review: The Name of the Wind",
+    },
+    {
+      id: "2",
+      title: "Game Review: Pokemon Brillian Diamond",
+    },
+    {
+      id: "3",
+      title: "Show Review: Alice in Borderland",
+    },
+  ]);
 });
-
 app.listen(4000, () => {
   console.log("Listening on Port 4000");
 });

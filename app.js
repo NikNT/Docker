877c931f9dbabd88d58c5817f9feae6bc79ef33d
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-10-21 23:45:28 -0400

    4. Docker - Volume and Notes

--- app.js
+++ app.js
@@ -6,20 +6,7 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json([
-    {
-      id: "1",
-      title: "Book Review: The Bear & The Nightingale",
-    },
-    {
-      id: "2",
-      title: "Game Review: Pokemon Brillian Diamond",
-    },
-    {
-      id: "3",
-      title: "Show Review: Alice in Borderland",
-    },
-  ]);
+  res.send("Hey World!");
 });
 
 app.listen(4000, () => {
